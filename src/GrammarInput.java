f39a2004e32cba54000a51dea2f2ed46fe87fce9
Cory Marleau <marleau24@up.edu>
2022-11-16 15:58:06 -0800

    Little bit of formatting

--- src/GrammarInput.java
+++ src/GrammarInput.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class GrammarInput {
     public static void main(String[] args){
         System.out.println("Please Enter a Filename: ");
@@ -13,7 +12,7 @@ public class GrammarInput {
         chomsky.toString();
         PDA pda = toPDA(chomsky);
         pda.toString();
-        System.out.println("we have therefore converted from a general grammar to a chomsky normal form grammar to a PDA");
+        System.out.println("We have therefore converted from a general grammar to a chomsky normal form grammar to a PDA");
         //TODO: Transformation to Chomsky
         //TODO: Transformation to PDA
         System.out.print(output.toString());
@@ -40,9 +39,9 @@ public class GrammarInput {
     }
     public static Grammar storeValue(){
         Grammar gram = new Grammar();
-        System.out.println("in this method we will store the parsed values into a grammar object that contains the variable " +
-                "\n  as well as a 2d Arraylist that contains the all of the rules from a given variable and if epsilons are \n" +
-                "found then ignore and continue, all of these grammar objects will be stored in a global arraylist\n");
+        System.out.println("In this method we will store the parsed values into a grammar object that contains the variable \n" +
+                            "as well as a 2d Arraylist that contains the all of the rules from a given variable and if epsilons are \n" +
+                            "found then ignore and continue, all of these grammar objects will be stored in a global arraylist\n");
         return gram;
 
     }
@@ -50,23 +49,23 @@ public class GrammarInput {
     public static Grammar Chomsky(Grammar gram){
         System.out.println("In this method we will be applying Chomsky Normal form to a given Grammar by:\n" +
                 "1. A new starting grammar object that contains the original start variable \n" +
-                "2. then we traverse through the global arraylist and check to see if there are any grammars that have an arraylist of one \n" +
-                "and are the same as the variable if so delete that arraylist\n" +
-                "3. then we traverse the global arraylist and if there are any arraylist with size one that also contain a variable \n" +
-                "then we copy that variable's arraylist and add it to the grammar that originally contained the arraylist shown above\n" +
+                "2. Then we traverse through the global arraylist and check to see if there are any grammars that have an arraylist of one \n" +
+                "   and are the same as the variable if so delete that arraylist\n" +
+                "3. Then we traverse the global arraylist and if there are any arraylist with size one that also contain a variable \n" +
+                "   then we copy that variable's arraylist and add it to the grammar that originally contained the arraylist shown above\n" +
                 "4. We then traverse and check to see if there are any arraylists that have a size larger than three if so then create size/2 new grammars \n" +
-                "storing the new grammar rules accordingly \n" +
-                "5. then traverse and create a new arraylist that contains all of the terminals, then traverse and check\n" +
-                "for any instance of the terminal and replace with a new grammar containing the single terminal\n");
+                "   storing the new grammar rules accordingly \n" +
+                "5. Then traverse and create a new arraylist that contains all of the terminals, then traverse and check\n" +
+                "   for any instance of the terminal and replace with a new grammar containing the single terminal\n");
         return gram;
     }
     public static PDA toPDA(Grammar gram){
         PDA ret = new PDA();
-        System.out.println("to convert to PDA is simple the set of states only contains {start,Q1,Qloop and Qaccept}\n" +
-                "the input alphabet contains all of the terminals, and the stack alphabet contains input alphabet, $, and variables\n" +
-                "the table would just be start going to Q1 on an epsilon and push $ then from Q1 to Qloop on an epsilon and push the start variable \n" +
-                "then on Qloop we create all of the loops with terminals we pop a terminal, else we pop a variable then push remaining values within array\n" +
-                "then from Qloop we pop a $ and go to Q accept after that the start state is always start and accept state is always Qaccept");
+        System.out.println("To convert to PDA is simple the set of states only contains {start,Q1,Qloop and Qaccept}\n" +
+                "The input alphabet contains all of the terminals, and the stack alphabet contains input alphabet, $, and variables\n" +
+                "The table would just be start going to Q1 on an epsilon and push $ then from Q1 to Qloop on an epsilon and push the start variable \n" +
+                "Then on Qloop we create all of the loops with terminals we pop a terminal, else we pop a variable then push remaining values within array\n" +
+                "Then from Qloop we pop a $ and go to Q accept after that the start state is always start and accept state is always Qaccept");
         return ret;
 
     }
