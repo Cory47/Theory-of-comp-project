f9a0059acd4c4dd12a89dee01e0243dbfb359092
pham24 <pham24@up.edu>
2022-11-16 16:02:07 -0800

    Did skeleton of proj

--- src/Grammar.java
+++ src/Grammar.java
@@ -1,6 +1,6 @@
 public class Grammar {
     @Override
     public String toString() {
-        return "*insert grammar here";
+        return "*insert grammar here*\n";
     }
 }
--- src/PDA.java
+++ src/PDA.java
@@ -1,6 +1,6 @@
 public class PDA {
     @Override
     public String toString() {
-        return "*insert pda here*";
+        return "*insert pda here*\n";
     }
 }
